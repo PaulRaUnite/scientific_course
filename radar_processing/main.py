dc3c4c5491715e3c77e66c95034478e37990e0c9
Pavlo Tokariev <pavlo.tokariev@inria.fr>
2022-03-25 11:31:54 +0100

    Init

---
+++ radar_processing/main.py
@@ -0,0 +1,51 @@
+# 1. Average 16 measurements
+# 2. compute FFT (fast furier transform) of the average
+# 3.
+# 4. look at
+#    the data
+#    the background data
+#    the subtraction
+import scipy.io
+import numpy as np
+import matplotlib.pyplot as plot
+
+
+def proc(prefix: str) -> (np.ndarray, np.ndarray):
+    data = np.zeros((16, 3202), dtype=int)
+    for i in range(0, 16):
+        n = i + 1
+        data[i] = scipy.io.loadmat(f"./data/{prefix}{n:02}.mat")["DATA_ORIGINAL"][1]
+    data = np.fft.fft(data)
+    averaged = np.average(data, axis=0)
+
+    fft = np.abs(averaged)
+    half = fft.shape[0] // 2
+    print(half)
+    plot.plot(fft)
+    plot.show()
+
+    return 20 * np.log10((2 / 50) * fft[0:half])
+
+
+if __name__ == '__main__':
+    with_object_frequencies = proc("donnees_")
+    background_frequencies = proc("donnees_vide_")
+    with_object_frequencies = with_object_frequencies
+    background_frequencies = background_frequencies
+
+    speed_of_light = 299792458  # [m / s]
+    slope = 1950037684072.203400  # [Hz / s]
+    Fs = 6250000
+    half = with_object_frequencies.shape[0]
+    frequency = np.linspace(0, (Fs / 2), half)
+    distance = (speed_of_light * frequency) / (2 * slope)
+
+    plot.plot(distance, with_object_frequencies)
+    plot.show(figsize=(12, 8))
+
+    plot.plot(distance, background_frequencies)
+    plot.show()
+
+    without_background = with_object_frequencies - background_frequencies
+    plot.plot(distance[0:100], without_background[0:100])
+    plot.show()
